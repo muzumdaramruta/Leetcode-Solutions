003ccba88565ab2b5b737ee88468826d348790b5
muzumdaramruta <m.amruta789@gmail.com>
2025-10-10 17:54:00 -0400

    Time: 54 ms (57.52%) | Memory: 21.1 MB (96.15%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.py
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.py
@@ -6,15 +6,21 @@
 #         self.right = None
 
 class Solution:
-    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        targets = {p, q}
-        def dfs(node):
-            if not node or node in targets:
-                return node
-            L = dfs(node.left)
-            R = dfs(node.right)
-            if L and R:
-                return node
-            return L or R
-
-        return dfs(root)
+    def lowestCommonAncestor(self, root, p, q):
+        stack = [root]
+        parent = {root: None}
+        while p not in parent or q not in parent:
+            node = stack.pop()
+            if node.left:
+                parent[node.left] = node
+                stack.append(node.left)
+            if node.right:
+                parent[node.right] = node
+                stack.append(node.right)
+        ancestors = set()
+        while p:
+            ancestors.add(p)
+            p = parent[p]
+        while q not in ancestors:
+            q = parent[q]
+        return q
