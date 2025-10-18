a30998600b55ea5ccad98befaf89a05415fcb85a
muzumdaramruta <m.amruta789@gmail.com>
2025-10-18 12:25:50 -0400

    Time: 41 ms (71.91%) | Memory: 18.4 MB (16.07%) - LeetSync

--- 133-clone-graph/clone-graph.py
+++ 133-clone-graph/clone-graph.py
@@ -8,15 +8,21 @@ class Node:
 
 from typing import Optional
 class Solution:
-    def dfs(self, node: 'Node', seen: dict[int, 'Node']) -> 'Node':
-        root: 'Node' = Node(node.val)
-        seen[node.val] = root
-        for neighbor in node.neighbors:
-            if neighbor.val not in seen: root.neighbors.append(self.dfs(neighbor, seen))
-            else: root.neighbors.append(seen[neighbor.val])
-        return root
-
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
-        if not node: return None
-        seen: dict[int, 'Node'] = dict()
-        return self.dfs(node, seen)
+
+        if not node:
+            return 
+            
+        visited = {node: Node(val=node.val)}
+        st = [node]
+        
+        while st:
+            src = st.pop()
+            copy = visited[src]
+            for n in src.neighbors:
+                if n not in visited:
+                    visited[n] = Node(val=n.val)
+                    st.append(n)
+                copy.neighbors.append(visited[n])
+        
+        return visited[node]
