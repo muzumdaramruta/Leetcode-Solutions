af813e94b518884ae0aea138cb8e2d6ad948a32c
muzumdaramruta <m.amruta789@gmail.com>
2025-10-18 12:23:39 -0400

    Time: 46 ms (37.06%) | Memory: 18.1 MB (82.55%) - LeetSync

--- 133-clone-graph/clone-graph.py
+++ 133-clone-graph/clone-graph.py
@@ -9,20 +9,19 @@ class Node:
 from typing import Optional
 class Solution:
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
-
         if not node:
-            return 
-            
-        visited = {node: Node(val=node.val)}
-        st = [node]
+            return
+        copied = {}
+
+        def dfs(curr):
+            if curr in copied:
+                return copied[curr]
+            clone = Node(curr.val, [])
+            copied[curr] = clone
+
+            for neighbor in curr.neighbors:
+                clone_neighbors = dfs(neighbor)
+                clone.neighbors.append(clone_neighbors)
+            return clone
         
-        while st:
-            src = st.pop()
-            copy = visited[src]
-            for n in src.neighbors:
-                if n not in visited:
-                    visited[n] = Node(val=n.val)
-                    st.append(n)
-                copy.neighbors.append(visited[n])
-        
-        return visited[node]
+        return dfs(node)
