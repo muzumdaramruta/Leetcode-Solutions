91e81b81052c2535bdc8533da0d6899e0fef29b2
muzumdaramruta <m.amruta789@gmail.com>
2025-10-18 12:22:51 -0400

    Time: 38 ms (85.72%) | Memory: 18 MB (82.55%) - LeetSync

--- 133-clone-graph/clone-graph.py
+++ 133-clone-graph/clone-graph.py
@@ -10,18 +10,19 @@ from typing import Optional
 class Solution:
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
 
-        if not node: return node
+        if not node:
+            return 
+            
+        visited = {node: Node(val=node.val)}
+        st = [node]
         
-        q, clones = deque([node]), {node.val: Node(node.val, [])}
-        while q:
-            cur = q.popleft() 
-            cur_clone = clones[cur.val]            
-
-            for ngbr in cur.neighbors:
-                if ngbr.val not in clones:
-                    clones[ngbr.val] = Node(ngbr.val, [])
-                    q.append(ngbr)
-                    
-                cur_clone.neighbors.append(clones[ngbr.val])
-                
-        return clones[node.val]
+        while st:
+            src = st.pop()
+            copy = visited[src]
+            for n in src.neighbors:
+                if n not in visited:
+                    visited[n] = Node(val=n.val)
+                    st.append(n)
+                copy.neighbors.append(visited[n])
+        
+        return visited[node]
