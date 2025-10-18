6c157c11de6ff9c693b28a6d30b4152661666184
muzumdaramruta <m.amruta789@gmail.com>
2025-10-18 12:32:46 -0400

    Time: 255 ms (44.59%) | Memory: 20.3 MB (57.48%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,21 +1,24 @@
+from collections import deque
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
+        """
+        :type grid: List[List[str]]
+        :rtype: int
+        """
         if not grid:
-            return 0
-            
+            return 0   
         count = 0
+        check = [[False for _ in range(len(grid[0]))] for _ in range(len(grid))]
         for i in range(len(grid)):
             for j in range(len(grid[0])):
-                if grid[i][j] == '1':
-                    self.dfs(grid, i, j)
+                if grid[i][j] =='1' and check[i][j]== False:
                     count += 1
-        return count
-
-    def dfs(self, grid, i, j):
-        if i<0 or j<0 or i>=len(grid) or j>=len(grid[0]) or grid[i][j] != '1':
-            return
-        grid[i][j] = '#'
-        self.dfs(grid, i+1, j)
-        self.dfs(grid, i-1, j)
-        self.dfs(grid, i, j+1)
-        self.dfs(grid, i, j-1)
+                    self.search(grid,check,i,j)
+        return count       
+    def search(self,grid,check,i,j):
+        qu = deque([(i,j)])
+        while qu:
+            i, j = qu.popleft()
+            if 0<=i<len(grid) and 0<=j<len(grid[0]) and grid[i][j]=='1' and check[i][j]==False:
+                check[i][j] = True
+                qu.extend([(i-1,j),(i+1,j),(i,j-1),(i,j+1)])
