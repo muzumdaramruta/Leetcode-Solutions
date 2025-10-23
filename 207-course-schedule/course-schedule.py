26280bc93026f6ee88076da46931db2fcefca93f
muzumdaramruta <m.amruta789@gmail.com>
2025-10-23 05:46:14 -0400

    Time: 0 ms (100.00%) | Memory: 19 MB (82.54%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -1,27 +1,22 @@
 class Solution:
+    def detectCycle(self, v, src, rst, vis):
+        vis[src] = 1
+        rst[src] = 1
+        for x in v[src]:
+            if vis[x] == 0 and self.detectCycle(v, x, rst, vis):
+                return True
+            elif rst[x] == 1:
+                return True
+        rst[src] = 0
+        return False
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        preMap = defaultdict(list)
-        for course, prereq in prerequisites:
-            preMap[course].append(prereq)
-
-        visiting = set()
-        visited = set()
-
-        def dfs(course):
-            if course in visiting:
-                return False  # cycle detected
-            if course in visited:
-                return True   # already checked
-
-            visiting.add(course)
-            for prereq in preMap[course]:
-                if not dfs(prereq):
-                    return False
-            visiting.remove(course)
-            visited.add(course)
-            return True
-
-        for course in range(numCourses):
-            if not dfs(course):
+        v = [[] for _ in range(numCourses)]
+        vis = [0] * numCourses
+        rst = [0] * numCourses
+        for x in prerequisites:
+            v[x[1]].append(x[0])
+        for i in range(numCourses):
+            if vis[i] == 0 and self.detectCycle(v, i, rst, vis):
                 return False
         return True
+        
