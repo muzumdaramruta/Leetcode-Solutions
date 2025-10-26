2e13e10c9cc56aa724c4789476c64fcfb691d54f
muzumdaramruta <m.amruta789@gmail.com>
2025-10-26 15:53:38 -0400

    Time: 15 ms (89.24%) | Memory: 17.8 MB (96.88%) - LeetSync

--- 945-snakes-and-ladders/snakes-and-ladders.py
+++ 945-snakes-and-ladders/snakes-and-ladders.py
@@ -1,28 +1,25 @@
-from collections import deque
 class Solution:
     def snakesAndLadders(self, board: List[List[int]]) -> int:
-        m,n=len(board),len(board[0])
-        grid=dict()
-        value=1
-        board.reverse()
-        t=0
-        for i in range(0,m):
-            for j in range(0,n):
-                if(t%2==0):
-                    grid[value]=board[i][j] if board[i][j]!=-1 else value
-                else:
-                    grid[value]=board[i][n-j-1] if board[i][n-j-1]!=-1 else value
-                value+=1
-            t+=1
-        value-=1
-        queue=deque()
-        queue.append((1,0))
-        while(queue):
-            index,c=queue.popleft()
-            for r in range(1,7):
-                if(index+r in grid and grid[index+r]!=0):
-                    if(grid[index+r]==value):
-                        return c+1
-                    queue.append((grid[index+r],c+1))
-                    grid[index+r]=0
+        n=len(board)
+        flatten=[0]*(n*n)
+        for i in range(n-1, -1, -2):
+            for j in range(n):
+                flatten[(n-1-i)*n+j]=board[i][j]-1
+                if i>=1:
+                    flatten[(n-i)*n+(n-1-j)]=board[i-1][j]-1
+        visited=[-1]*(n*n)
+        q=deque()
+        q.append(0)
+        visited[0]=0
+        while q:
+            curr=q.popleft()
+            if curr==n*n-1:
+                return visited[curr]
+            i0=min(6, n*n-1-curr)
+            for i in range(1, i0+1):
+                next=curr+i
+                if flatten[next]!=-2: next=flatten[next]
+                if visited[next]!=-1: continue
+                visited[next]=visited[curr]+1
+                q.append(next)
         return -1
