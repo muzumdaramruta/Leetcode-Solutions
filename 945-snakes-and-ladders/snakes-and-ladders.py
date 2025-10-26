a56a8f9aec6430c50a2a6b4c0c78465babea4238
muzumdaramruta <m.amruta789@gmail.com>
2025-10-26 15:54:54 -0400

    Time: 18 ms (78.94%) | Memory: 18.1 MB (39.21%) - LeetSync

--- 945-snakes-and-ladders/snakes-and-ladders.py
+++ 945-snakes-and-ladders/snakes-and-ladders.py
@@ -1,25 +1,26 @@
 class Solution:
     def snakesAndLadders(self, board: List[List[int]]) -> int:
-        n=len(board)
-        flatten=[0]*(n*n)
-        for i in range(n-1, -1, -2):
-            for j in range(n):
-                flatten[(n-1-i)*n+j]=board[i][j]-1
-                if i>=1:
-                    flatten[(n-i)*n+(n-1-j)]=board[i-1][j]-1
-        visited=[-1]*(n*n)
-        q=deque()
-        q.append(0)
-        visited[0]=0
-        while q:
-            curr=q.popleft()
-            if curr==n*n-1:
-                return visited[curr]
-            i0=min(6, n*n-1-curr)
-            for i in range(1, i0+1):
-                next=curr+i
-                if flatten[next]!=-2: next=flatten[next]
-                if visited[next]!=-1: continue
-                visited[next]=visited[curr]+1
-                q.append(next)
+        n = len(board)
+        def label_to_position(label):
+            r, c = divmod(label-1, n)
+            if r % 2 == 0:
+                return n-1-r, c
+            else:
+                return n-1-r, n-1-c
+            
+        seen = set()
+        queue = collections.deque()
+        queue.append((1, 0))
+        while queue:
+            label, step = queue.popleft()
+            r, c = label_to_position(label)
+            if board[r][c] != -1:
+                label = board[r][c]
+            if label == n*n:
+                return step
+            for x in range(1, 7):
+                new_label = label + x
+                if new_label <= n*n and new_label not in seen:
+                    seen.add(new_label)
+                    queue.append((new_label, step+1))
         return -1
