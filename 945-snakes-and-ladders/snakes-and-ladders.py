3c04502581eb496d0227738162e03794354b0230
muzumdaramruta <m.amruta789@gmail.com>
2025-10-26 15:52:36 -0400

    Time: 15 ms (89.24%) | Memory: 18.2 MB (16.72%) - LeetSync

--- 945-snakes-and-ladders/snakes-and-ladders.py
+++ 945-snakes-and-ladders/snakes-and-ladders.py
@@ -1,35 +1,28 @@
 from collections import deque
-
 class Solution:
-    def snakesAndLadders(self, board):
-        n = len(board)
-        
-        def get_pos(num):
-            r, c = divmod(num - 1, n)
-            if r % 2 == 0:
-                return (n - 1 - r, c)
-            return (n - 1 - r, n - 1 - c)
-
-        visited = [False] * (n * n + 1)
-        q = deque([1])
-        visited[1] = True
-        moves = 0
-
-        while q:
-            for _ in range(len(q)):
-                curr = q.popleft()
-                if curr == n * n:
-                    return moves
-                for i in range(1, 7):
-                    nxt = curr + i
-                    if nxt > n * n:
-                        continue
-                    r, c = get_pos(nxt)
-                    if board[r][c] != -1:
-                        nxt = board[r][c]
-                    if not visited[nxt]:
-                        visited[nxt] = True
-                        q.append(nxt)
-            moves += 1
-
+    def snakesAndLadders(self, board: List[List[int]]) -> int:
+        m,n=len(board),len(board[0])
+        grid=dict()
+        value=1
+        board.reverse()
+        t=0
+        for i in range(0,m):
+            for j in range(0,n):
+                if(t%2==0):
+                    grid[value]=board[i][j] if board[i][j]!=-1 else value
+                else:
+                    grid[value]=board[i][n-j-1] if board[i][n-j-1]!=-1 else value
+                value+=1
+            t+=1
+        value-=1
+        queue=deque()
+        queue.append((1,0))
+        while(queue):
+            index,c=queue.popleft()
+            for r in range(1,7):
+                if(index+r in grid and grid[index+r]!=0):
+                    if(grid[index+r]==value):
+                        return c+1
+                    queue.append((grid[index+r],c+1))
+                    grid[index+r]=0
         return -1
