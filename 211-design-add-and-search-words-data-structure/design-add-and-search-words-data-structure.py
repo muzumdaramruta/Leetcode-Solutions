57b1432551e773dd5f201ec688b9386ae27cd770
muzumdaramruta <m.amruta789@gmail.com>
2025-10-28 08:22:21 -0400

    Time: 1388 ms (31.16%) | Memory: 52.1 MB (94.25%) - LeetSync

--- 211-design-add-and-search-words-data-structure/design-add-and-search-words-data-structure.py
+++ 211-design-add-and-search-words-data-structure/design-add-and-search-words-data-structure.py
@@ -1,40 +1,18 @@
-class TrieNode:
-    def __init__(self):
-        self.children={}
-        self.endOfWord=False
 class WordDictionary:
 
     def __init__(self):
-        self.root=TrieNode()
+        self.root = {}
+    
+    def addWord(self, word):
+        node = self.root
+        for char in word:
+            node = node.setdefault(char, {})
+        node['$'] = None
 
-    def addWord(self, word: str) -> None:
-        cur=self.root
-        for letter in word:
-            if letter not in cur.children:
-                cur.children[letter]=TrieNode()
-            cur=cur.children[letter]
-        cur.endOfWord=True
-
-    def search(self, word: str) -> bool:
-        def dfs(j,root):
-            cur=root
-            for i in range(j,len(word)):
-                letter=word[i]
-                if letter=='.':
-                    for child in cur.children.values():
-                        if dfs(i+1,child):
-                            return True
-                    return False
-                else:
-                    if letter not in cur.children:
-                        return False
-                    cur=cur.children[letter]
-            return cur.endOfWord
-        return dfs(0,self.root)
-
-
-
-# Your WordDictionary object will be instantiated and called as such:
-# obj = WordDictionary()
-# obj.addWord(word)
-# param_2 = obj.search(word)
+    def search(self, word):
+        nodes = [self.root]
+        for char in word + '$':
+            nodes = [kid for node in nodes for kid in
+                     ([node[char]] if char in node else
+                      filter(None, node.values()) if char == '.' else [])]
+        return bool(nodes)
