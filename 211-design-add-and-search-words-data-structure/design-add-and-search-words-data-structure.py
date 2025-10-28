99ab51e2b857d9bea4ea405eb0d34a112f60de39
muzumdaramruta <m.amruta789@gmail.com>
2025-10-28 08:25:05 -0400

    Time: 5920 ms (5.01%) | Memory: 25.1 MB (99.76%) - LeetSync

--- 211-design-add-and-search-words-data-structure/design-add-and-search-words-data-structure.py
+++ 211-design-add-and-search-words-data-structure/design-add-and-search-words-data-structure.py
@@ -1,29 +1,22 @@
-class WordDictionary:
-
+class WordDictionary(object):
     def __init__(self):
-        self.trie = dict()
+        self.word_dict = collections.defaultdict(list)
+        
 
-    def addWord(self, word: str) -> None:
-        node = self.trie
-        for ch in word + '\U0001f33b':
-            if ch not in node:
-                node[ch] = dict()
+    def addWord(self, word):
+        if word:
+            self.word_dict[len(word)].append(word)
 
-            node = node[ch]
-
-    def search(self, word: str) -> bool:
-        nodes = [self.trie]
-        for ch in word + '\U0001f33b':
-            newNodes = []
-            for node in nodes:
-                if ch == '.': 
-                    newNodes += [v for v in node.values()]
-                elif ch in node: 
-                    newNodes.append(node[ch])
-
-            if not newNodes:
-                return False
-
-            nodes = newNodes
-            
-        return True
+    def search(self, word):
+        if not word:
+            return False
+        if '.' not in word:
+            return word in self.word_dict[len(word)]
+        for v in self.word_dict[len(word)]:
+            # match xx.xx.x with yyyyyyy
+            for i, ch in enumerate(word):
+                if ch != v[i] and ch != '.':
+                    break
+            else:
+                return True
+        return False
