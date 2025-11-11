c69d3457fbb82883e359a5b2addb23dce090cfb2
muzumdaramruta <m.amruta789@gmail.com>
2025-11-10 20:38:39 -0500

    Time: 171 ms (70.21%) | Memory: 32.9 MB (67.87%) - LeetSync

--- 148-sort-list/sort-list.py
+++ 148-sort-list/sort-list.py
@@ -7,34 +7,23 @@ class Solution:
     def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         if not head or not head.next:
             return head
-        
-        # Find the middle of the linked list 
-        slow, fast = head, head.next
+        slow = head
+        fast = head.next
         while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
-        
         mid = slow.next
         slow.next = None
-        
-        # Sort both halves
         left = self.sortList(head)
         right = self.sortList(mid)
-        
-        return self.merge(left, right)
-    
-    def merge(self, l1: ListNode, l2: ListNode) -> ListNode:
-        dummy = ListNode(0)
-        tail = dummy
-        
+        return self.merge(left,right)
+    def merge(self,l1,l2):
+        dummy = tail = ListNode()
         while l1 and l2:
-            if l1.val < l2.val:
-                tail.next = l1
-                l1 = l1.next
+            if l1.val<l2.val:
+                tail.next,l1 = l1,l1.next
             else:
-                tail.next = l2
-                l2 = l2.next
+                tail.next,l2 = l2,l2.next
             tail = tail.next
-        
-        tail.next = l1 if l1 else l2
+        tail.next = l1 or l2
         return dummy.next
