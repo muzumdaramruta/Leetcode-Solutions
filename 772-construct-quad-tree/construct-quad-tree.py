f686ce0a2540fd164a7c9d905a05591021bc1cc5
muzumdaramruta <m.amruta789@gmail.com>
2025-11-12 00:38:01 -0500

    Time: 99 ms (35.33%) | Memory: 18.5 MB (68.98%) - LeetSync

--- 772-construct-quad-tree/construct-quad-tree.py
+++ 772-construct-quad-tree/construct-quad-tree.py
@@ -1,30 +1,34 @@
+"""
+# Definition for a QuadTree node.
 class Node:
-    def __init__(self, val, isLeaf, topLeft=None, topRight=None, bottomLeft=None, bottomRight=None):
+    def __init__(self, val, isLeaf, topLeft, topRight, bottomLeft, bottomRight):
         self.val = val
         self.isLeaf = isLeaf
         self.topLeft = topLeft
         self.topRight = topRight
         self.bottomLeft = bottomLeft
         self.bottomRight = bottomRight
+"""
 
 class Solution:
-    def construct(self, grid: List[List[int]]) -> Node:
-        return self.helper(grid, 0, 0, len(grid))
+    def construct(self, grid: List[List[int]]) -> 'Node':
+        def construct_range(top, left, l):
+            """
+            Construct a node representing the subgrid with
+            top left corner at (top, left) and side length l
+            """
+            if l == 1:
+                return Node(grid[top][left], True)
+            topLeft = construct_range(top, left, l // 2)
+            topRight = construct_range(top, left + l // 2, l // 2)
+            botLeft = construct_range(top + l // 2, left, l // 2)
+            botRight = construct_range(top + l // 2, left + l // 2, l // 2)
 
-    def helper(self, grid, i, j, w):
-        if self.allSame(grid, i, j, w):
-            return Node(grid[i][j] == 1, True)
-
-        node = Node(True, False)
-        node.topLeft = self.helper(grid, i, j, w // 2)
-        node.topRight = self.helper(grid, i, j + w // 2, w // 2)
-        node.bottomLeft = self.helper(grid, i + w // 2, j, w // 2)
-        node.bottomRight = self.helper(grid, i + w // 2, j + w // 2, w // 2)
-        return node
-
-    def allSame(self, grid, i, j, w):
-        for x in range(i, i + w):
-            for y in range(j, j + w):
-                if grid[x][y] != grid[i][j]:
-                    return False
-        return True
+            children = [topLeft, topRight, botLeft, botRight]
+            # Check if all subgrids have the same value
+            if all(child.isLeaf and child.val == topLeft.val for child in children):
+                return Node(topLeft.val, True)
+            
+            return Node(0, False, topLeft, topRight, botLeft, botRight)
+        
+        return construct_range(0, 0, len(grid))
