5fc87df925b8fc99f887d8ea913269e7eaa7545e
muzumdaramruta <m.amruta789@gmail.com>
2025-11-12 00:40:07 -0500

    Time: 96 ms (47.66%) | Memory: 18.6 MB (68.98%) - LeetSync

--- 772-construct-quad-tree/construct-quad-tree.py
+++ 772-construct-quad-tree/construct-quad-tree.py
@@ -12,23 +12,5 @@ class Node:
 
 class Solution:
     def construct(self, grid: List[List[int]]) -> 'Node':
-        def construct_range(top, left, l):
-            """
-            Construct a node representing the subgrid with
-            top left corner at (top, left) and side length l
-            """
-            if l == 1:
-                return Node(grid[top][left], True)
-            topLeft = construct_range(top, left, l // 2)
-            topRight = construct_range(top, left + l // 2, l // 2)
-            botLeft = construct_range(top + l // 2, left, l // 2)
-            botRight = construct_range(top + l // 2, left + l // 2, l // 2)
-
-            children = [topLeft, topRight, botLeft, botRight]
-            # Check if all subgrids have the same value
-            if all(child.isLeaf and child.val == topLeft.val for child in children):
-                return Node(topLeft.val, True)
-            
-            return Node(0, False, topLeft, topRight, botLeft, botRight)
-        
-        return construct_range(0, 0, len(grid))
+        if all(cell == grid[0][0] for row in grid for cell in row): return Node(bool(grid[0][0]), True)
+        return Node(True, False, self.construct([row[:len(grid) // 2] for row in grid[:len(grid) // 2]]), self.construct([row[len(grid) // 2:] for row in grid[:len(grid) // 2]]), self.construct([row[:len(grid) // 2] for row in grid[len(grid) // 2:]]), self.construct([row[len(grid) // 2:] for row in grid[len(grid) // 2:]]))
